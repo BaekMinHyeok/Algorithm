8059e9b45d3835d33bfd40cb652de1833654bc72
BaekMinHyeok <92504602+BaekMinHyeok@users.noreply.github.com>
2023-08-13 20:55:36 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.08 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,30 @@
+// function solution(numbers) {
+//     return numbers.reduce((a, b) => [...a, b * 2], []);
+// }
+
+const solution = (numbers) => numbers.map((number) => number * 2)
+
+
+
+// function solution(numbers) {
+//     return numbers.map(i=>i*2);
+// }
+
+
+
+// function solution(numbers) {
+//     var answer = [];
+//     for(var i=0; i<numbers.length;i++){
+//         answer[i]=numbers[i]*2;
+//     }
+
+//     return answer;
+// }
+
+
+
+// function solution(numbers) {
+//     var answer = [];
+//     numbers.forEach(number => answer.push(2*number))
+//     return answer;
+// }
