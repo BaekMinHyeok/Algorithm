4e16ab9284e37d704ef072e2fb030259dc968beb
BaekMinHyeok <92504602+BaekMinHyeok@users.noreply.github.com>
2023-09-12 18:32:42 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 2.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,9 +1,15 @@
+// function solution(my_string, n) {
+//     let answer = "";
+//     for (let i = 0; i < my_string.length;i++){
+//         for(let j = 0; j < n; j++) {
+//             answer += my_string.charAt(i)
+//         }
+//     }
+//     return answer
+// }
+
 function solution(my_string, n) {
-    let answer = "";
-    for (let i = 0; i < my_string.length;i++){
-        for(let j = 0; j < n; j++) {
-            answer += my_string.charAt(i)
-        }
-    }
-    return answer
+    const answer = [...my_string].map(v => v.repeat(n)).join("");
+    console.log(answer);
+    return answer;
 }
