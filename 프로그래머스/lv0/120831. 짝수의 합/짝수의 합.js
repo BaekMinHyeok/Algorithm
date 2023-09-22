494ef06ca1a79a879c90d9c18970e0ed8c17f5c1
BaekMinHyeok <92504602+BaekMinHyeok@users.noreply.github.com>
2023-09-22 12:13:53 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,17 @@
+// function solution(n) {
+//   let sum = 0;
+//     for(let i = 2; i <= n; i += 2){
+//         sum += i;
+//     }
+//     return sum;
+// }
+
 function solution(n) {
-  let sum = 0;
-    for(let i = 2; i <= n; i += 2){
-        sum += i;
-    }
-    return sum;
+    const half = ~~(n/2)
+    return half *(half + 1)
 }
+
+// function solution(n) {
+//     const half = Math.floor(n/2);
+//     return half*(half+1);
+// }
