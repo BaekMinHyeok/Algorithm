658d8050b50fcc520c0c2aac030703d8aca89ab6
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2023-11-03 17:07:54 +0900

    [level unrated] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.js
@@ -1,30 +1,37 @@
+// function solution(hp) {
+
+//   const generalAnt = 5;
+//   const soldierAnt = 3;
+//   const workerAnt = 1;
+  
+//   let generalAnts = 0;
+//   let soldierAnts = 0;
+//   let workerAnts = 0;
+  
+
+//   while (hp > 0) {
+//     if (hp >= generalAnt) {
+//       hp -= generalAnt;
+//       generalAnts++;
+//     } else if (hp >= soldierAnt) {
+//       hp -= soldierAnt;
+//       soldierAnts++;
+//     } else {
+//       hp -= workerAnt;
+//       workerAnts++;
+//     }
+//   }
+  
+
+//   const totalAnts = generalAnts + soldierAnts + workerAnts;
+//   return totalAnts;
+// }
+
+
 function solution(hp) {
-
-  const generalAnt = 5;
-  const soldierAnt = 3;
-  const workerAnt = 1;
-  
-  let generalAnts = 0;
-  let soldierAnts = 0;
-  let workerAnts = 0;
-  
-
-  while (hp > 0) {
-    if (hp >= generalAnt) {
-      hp -= generalAnt;
-      generalAnts++;
-    } else if (hp >= soldierAnt) {
-      hp -= soldierAnt;
-      soldierAnts++;
-    } else {
-      hp -= workerAnt;
-      workerAnts++;
-    }
-  }
-  
-
-  const totalAnts = generalAnts + soldierAnts + workerAnts;
-  return totalAnts;
+    const a = Math.floor(hp / 5);
+    const b = Math.floor((hp - (a * 5)) / 3);
+    const c = hp - ((a * 5) + (b * 3));
+    return a+b+c;
 }
-
 
