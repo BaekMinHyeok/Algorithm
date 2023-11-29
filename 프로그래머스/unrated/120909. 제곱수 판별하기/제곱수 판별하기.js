3851ea4dd3737a513a0f34cc2137463c3429d7fe
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2023-11-29 18:22:40 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,10 +1,11 @@
 function solution(n) {
     
-   let num = Math.sqrt(n)
+//    let num = Math.sqrt(n)
    
-   if(num % 1 === 0) {
-       return 1
-   } else {
-       return 2
-   }
+//    if(num % 1 === 0) {
+//        return 1
+//    } else {
+//        return 2
+//    }
+    return Math.sqrt(n) % 1 === 0 ? 1 : 2
 }
