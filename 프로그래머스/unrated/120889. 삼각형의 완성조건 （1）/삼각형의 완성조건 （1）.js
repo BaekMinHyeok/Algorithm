0c36008b7ec5f1bb9e2ac1d294842783a5927855
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2023-11-30 01:17:45 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,9 +1,14 @@
 function solution(sides) {
-    const maxNum = sides.indexOf(Math.max(...sides));
-    const sumOfOtherSides  = sides.reduce((a, b, c) => (c !== maxNum) ? a + b : a, 0)
-     if (sides[maxNum] < sumOfOtherSides ) {
-        return 1; 
-    } else {
-        return 2; 
-    }
+    // const maxNum = sides.indexOf(Math.max(...sides));
+    // const sumOfOtherSides  = sides.reduce((a, b, c) => (c !== maxNum) ? a + b : a, 0)
+    //  if (sides[maxNum] < sumOfOtherSides ) {
+    //     return 1; 
+    // } else {
+    //     return 2; 
+    // }
+    sides = sides.sort((a,b) => a-b)
+    return sides[0] + sides[1] > sides[2] ? 1 : 2;
 }
+
+ // sides = sides.sort((a,b) => a-b)
+ //    return sides[0]+sides[1] > sides[2] ? 1 : 2;
