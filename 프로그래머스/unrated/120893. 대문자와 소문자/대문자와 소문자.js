abe0813c522c2613a964bb0399cc4da7d14a0539
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2023-12-10 22:55:11 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.51 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    let answer = "";
+    for (let i = 0; i < my_string.length; i++) {
+        let char = my_string.charAt(i);
+        if (char === char.toLowerCase()) {
+            answer += char.toUpperCase();
+        } else {
+            answer += char.toLowerCase();
+        }
+    }
+    return answer
+}
