3b7c4848467b1263084555eb25cceebb861474cc
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2024-03-21 11:13:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.02 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,11 @@
 function solution(arr)
 {
-    let result = []
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== arr[i+1]){
-            result.push(arr[i])
-        }
-    }
-    return result
+    // let result = []
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i] !== arr[i+1]){
+    //         result.push(arr[i])
+    //     }
+    // }
+    // return result
+    return arr.filter((v,i) => v !== arr[i+1])
 }
