dec2b4842d43e2aab6f13bb1a7c64e46c9e179de
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2024-03-22 12:02:54 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+    d.sort((a,b)=> a-b)
+    let count = 0;
+    let sum = 0;
+    for(let i = 0; i < d.length; i++){
+        sum += d[i]
+        if(sum <= budget){
+            count++
+        }
+    }
+    if(sum <= budget){
+        count = d.length
+    }
+    return count
+}
