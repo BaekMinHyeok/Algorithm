0a9a970c5e597af596bc0f536b9efe62447c8aa8
백민혁 <92504602+BaekMinHyeok@users.noreply.github.com>
2024-03-29 11:18:01 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 12.91 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let arr = [];
+    let result = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if(!arr.includes(s[i])){
+            result.push(-1)
+            arr.push(s[i])
+        } else if(arr.includes(s[i])) {
+            result.push(i - arr.lastIndexOf(s[i]))
+            arr.push(s[i])
+        }
+    }
+    return result
+        
+}
